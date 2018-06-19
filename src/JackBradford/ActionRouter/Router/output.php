0de51efd45fa565b6524c8bbf68bc2138aeeb39b
Jack Brown <jack@jackbrown.io>
2018-06-19 11:42:38 -0400

    Added Router, Output classes
    I've added the Router and Output classes I developed for use with
    Atlantic Lab Equipment's inventory management system. I've added two new
    methods to the Router class:
    
    Router::init() serves the same purpose as the beginning of ALE IMS's
    index.php file: it checks that the user is logged in before creating an
    instance of the Router class. This is a static method which returns an
    instance of its containing class.
    
    Router::routeAndExecuteRequest() provides a simple method via which to
    route requests using the class. Other setups can be configured for use
    in lieu of this method via the rest of the Router API.

---
+++ src/JackBradford/ActionRouter/Router/output.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * @file output.php
+ * This file provides a class for handling the output of the application.
+ *
+ * @class Output
+ * This class is responsible for managing the output of the application, including
+ * capturing any output from e.g. output buffers, as well as managing the properties
+ * of the final page (in the case of synchronous page requests), like the title
+ * and meta description.
+ */
+class Output {
+
+	protected $content		=	null;
+	protected $templatePath	=	null;
+	protected $metaDesc		=	null;
+	protected $title		=	null;
+	protected $section		=	null;
+
+	private function __construct() {}
+	private function __clone() {}
+
+	/**
+	 * @method Output::setPageInfo()
+	 * Set the data associated with the requested page.
+	 *
+	 * @param array $info
+	 * An array containing the page information. Available nodes include 'title'
+	 * (page title), 'metaDesc' (page meta description), and 'section' (the
+	 * section of the site in which the page exists).
+	 *
+	 * @return void
+	 * Sets the appropriate properties of the class.
+	 */
+	public function setPageInfo($info) {
+
+		if (
+			!isset($info['title']) 
+			|| !isset($info['metaDesc']) 
+			|| !isset($info['section'])
+		) {
+
+			$m	=	__METHOD__ . " expects the \$info array to include 'title', ";
+			$m	.=	"'metaDesc', and 'section' nodes.";
+			throw new InvalidArgumentException($m, 408);
+		}
+
+		$this->setTitle($info['title']);
+		$this->setMetaDesc($info['metaDesc']);
+		$this->setSection($info['section']);
+	}
+
+	/**
+	 * @method Output::setContent()
+	 * Set the content of the page, or the content to be returned to the client
+	 * as part of an asynchronous request.
+	 *
+	 * @param str $content
+	 * HTML content to be stored until it is output.
+	 *
+	 * @return void
+	 * Sets the instance's $content property.
+	 */
+	public function setContent($content) {
+
+		if (!isset($content)) {
+
+			$m	=	__METHOD__ . ' Missing Argument: expects $content.';
+			throw new BadMethodCallException($m, 403);
+		}
+
+		$this->content = $content;
+	}
+
+	/**
+	 * @method Output::setTemplate()
+	 * Set the HTML template file to be used when serving content as part of a
+	 * synchronous page request.
+	 *
+	 * @param str $templatePath
+	 * The filepath of the HTML template to be used to serve the content.
+	 *
+	 * @return void
+	 * Sets the instance's $templatePath property.
+	 */
+	public function setTemplate($templatePath) {
+
+		if (!is_string($templatePath)) {
+
+			$m = __METHOD__ . ' expects argument $templatePath as string.';
+			throw new InvalidArgumentException($m, 404);
+		}
+
+		$this->templatePath	= $templatePath;
+	}
+
+	/**
+	 * @method Output::setTitle()
+	 * Set the title of the page. The HTML template specified will use this
+	 * title. Not applicable to asynchronous requests.
+	 *
+	 * @param str $title
+	 * The title of the page.
+	 *
+	 * @return void
+	 * Sets the instance's $title property.
+	 */
+	public function setTitle($title) {
+
+		if (!is_string($title)) {
+
+			$m = __METHOD__ . ' expects $title as string.';
+			throw new InvalidArgumentException($m, 405);
+		}
+
+		$this->title = htmlspecialchars($title);
+	}
+
+	/**
+	 * @method Output::setMetaDesc()
+	 * Set the meta description of the page. The HTML template specified should
+	 * use this description. Not applicable to asynchronous requests.
+	 *
+	 * @param str $metaDesc
+	 * The page's meta description.
+	 *
+	 * @return void
+	 * Sets the instance's $metaDesc property.
+	 */
+	public function setMetaDesc($metaDesc) {
+
+		if (!is_string($metaDesc)) {
+
+			$m = __METHOD__ . ' expects $metaDesc as string.';
+			throw new InvalidArgumentException($m, 406);
+		}
+
+		$this->metaDesc = htmlspecialchars($metaDesc);
+	}
+
+	/**
+	 * @method Output::setSection()
+	 * Set the current section of the site in which the page exists. The HTML
+	 * template specified will use this section. Not applicable to asynchronous
+	 * requests.
+	 *
+	 * @param str $section
+	 * The site section in which the requested page exists.
+	 *
+	 * @return void
+	 * Set the instance's $section property.
+	 */
+	public function setSection($section) {
+
+		if (!is_string($section)) {
+
+			$m = __METHOD__ . ' expects $section as string.';
+			throw new InvalidArgumentException($m, 407);
+		}
+
+		$this->section = htmlspecialchars($section);
+	}
+
+	/**
+	 * @method Output::flushContent()
+	 * Flush the content set in the $content property to the client. Flushes
+	 * only the content, without any template.
+	 *
+	 * @return void
+	 * Sends the contents of the instance's $content property to the client.
+	 */
+	protected function flushContent() {
+
+		if (empty($this->content)) {
+
+			$m = __METHOD__ . ': No content found.';
+			throw new BadMethodCallException($m, 408);
+		}
+
+		echo $this->content;
+	}
+
+	/**
+	 * @method Output::flushAll()
+	 * Flush the content set in the $content property within the template set in
+	 * the $templatePath property to the client.
+	 *
+	 * @return void
+	 * Sends the template, along with the contents of the instance's $content
+	 * property, to the client.
+	 */
+	protected function flushAll() {
+
+		if (empty($this->templatePath)) {
+
+			$m	=	__METHOD__ . ': No template path found.';
+			throw new BadMethodCallException($m, 401);
+		}
+
+		if (empty($this->content)) {
+
+			$m	=	__METHOD__ . ': No content found.';
+			throw new BadMethodCallException($m, 402);
+		}
+
+		require_once $this->templatePath;
+	}
+}
+
---
+++ src/JackBradford/ActionRouter/Router/router.php
@@ -0,0 +1,382 @@
+<?php
+/**
+ * @file
+ * This file provides a router for the application.
+ *
+ * The router loads a list of "routes," which map actions to their
+ * corresponding controllers. A route is specified with the request. This
+ * class has the responsibility of routing the request to the appropriate
+ * controller, which oversees the business logic of the requested task.
+ *
+ */
+class Router extends Output {
+
+	protected	$user;
+	private		$controller;
+	private		$action;
+	private		$db;
+	private		$request;
+	private		$logger;
+	private		$dc;
+	private		$serveContentOnly = false;
+	private		$routes = array();
+
+	/**
+	 * @method Router::__construct()
+	 * Construct a new instance of the Router class, which is responsible for
+	 * inspecting the request, identifying and validating the requested action,
+	 * calling the appropriate controller method, and returning the result in
+	 * the requested format.
+	 *
+	 * @param RoutingDIContainer $dc
+	 * An instance of the Routing Dependency Injection Container, which contains
+	 * a Request object, Logger object, Db Abstraction object, and a User Management
+	 * object.
+	 */
+	public function __construct(RoutingDIContainer $dc) {
+
+		try {
+	
+			$this->dc		=	$dc;
+			$this->request	=	$dc->request;
+			$this->logger	=	$dc->logger;
+			$this->db		=	$dc->db;
+			$this->user		=	$dc->user;
+			$this->routes 	=	$this->loadRoutes();
+		}
+		catch (Exception $e) {
+
+			if (isset($dc->logger) && ($dc->logger instanceof Logger)) {
+
+				$dc->logger->logError($e);
+			}
+			$m	=	'Could not construct Router.';
+			throw new Exception($m, 0, $e);
+		}
+	}
+
+	/**
+	 * @method Router::init()
+	 * Initialize the Router in preparation of handling the current request.
+	 *
+	 * @param str $configPath
+	 * The path to the configuration file.
+	 *
+	 * @return Router
+	 */
+	public static function init($configPath) {
+
+		try {
+
+			require_once $configPath;
+			$request	=	new Request();
+			$settings	=	new PublicSettings();
+			$user		=	new UserManager($request, $settings);
+
+			if (!$user->isLoggedIn()) {
+
+				if	 	($request->isFromCLI()) $user->loginCLIAdmin();
+				elseif 	(!$request->isAuthRequest()) {
+
+					$message	=	'User must log in; request was not for authorization. ';
+					$message	.=	'Asking user to send authorization request...';
+					throw new NotLoggedInException($message);
+				}
+			}
+
+			$db			=	DbFactory::getDbInst();
+			$logger		=	new Logger($db);
+			$dc			=	new RoutingDIContainer($request, $db, $logger, $user);
+
+			return new Router($dc);
+
+		} catch (NotLoggedInException $e) {
+
+			$login = ($request->isAsync()) ? 'askForAsyncLogin' : 'sendToLoginPage';
+			UserManager::{ $login }();
+
+		} catch (Exception $e) {
+
+			$m	=	$e->getMessage() . ' ' . $e->getTraceAsString();
+			error_log('ActionRouter failed to initialize: '.$m);
+		}
+	}
+
+	/**
+	 * @method Router::routeAndExecuteRequest()
+	 * Route the request to the appropriate controller action, then execute
+	 * that action.
+	 *
+	 * @param bool $serveClientAppOnSync
+	 * Specify whether the router should serve the HTML necessary to run the
+	 * client application, rather than the requested data, if the request is
+	 * synchronous.
+	 *
+	 * @return void
+	 * Emits a JSON object (or the HTML necessary to run the client 
+	 * application).
+	 */
+	public function routeAndExecuteRequest($serveClientAppOnSync=true) {
+
+		try {
+
+			$ctrlModel	=	CONTROLLERS[$this->request->getNameOfRequestedController()];
+			$controller	=	new $ctrlModel($this->router, $this->dc);
+
+			$this->setController($controller);
+			$this->setControllerAction($this->request->getNameOfRequestedAction());
+
+			$result	= (!$this->request->isAsync() && $serveClientAppOnSync)
+				? $this->callClientApp()
+				: $this->callRequestedAction();
+
+			$this->setContent($result);
+			$this->serveContent();
+
+		} catch (Exception $e) {
+
+			$this->logger->logError($e);
+			$this->serveErrorNotice($e);
+		}
+	}
+
+	/**
+	 * @method Router::callClientApp()
+	 * In some instances, depending on client configuration, it may be
+	 * advantageous to serve the client app itself rather than the data
+	 * requested. Via such configuration, the same URL can be used both to 
+	 * resolve the initial request (by serving the client app only) and 
+	 * to deliver the data necessary in that context when called subsequently
+	 * by that delivered client.
+	 *
+	 * @return void
+	 */
+	public function callClientApp() {
+
+		switch ($this->request->getNameOfRequestedController()) {
+
+			case 'admin':
+			case 'record_request':
+				$this->setTemplate(ADMIN_TEMPLATE);
+				break;
+
+			default:
+				$this->setTemplate(PUBLIC_TEMPLATE);
+		}
+		require_once TEMPLATE_PATH . '/admin_client.php';
+		return ob_get_clean();
+	}
+
+	/**
+	 * @method callRequestedAction()
+	 * Call the action specified in the request via the appropriate controller.
+	 *
+	 * @return string
+	 * Returns the contents of the output buffer, which will have collected the
+	 * output from the controller's method call. The output buffer was started
+	 * when the application's config file was loaded.
+	 */
+	public function callRequestedAction() {
+
+		if (isset($this->action)) $action = $this->action;
+		else throw new Exception(__METHOD__.': No Action Given.', 302);
+
+		$this->controller->{ $action }();
+		$content =	ob_get_clean();
+
+		if ($content === false) {
+
+			$message = __METHOD__ . ': Could not get content from buffer.';
+			throw new Exception($message, 306);
+		}
+		return $content;
+	}
+	
+	/**
+	 * @method Router::getActionName()
+	 * Get the name of the currently-set action which will/has been run by the
+	 * controller.
+	 *
+	 * @return str
+	 * Returns the name of the action.
+	 */
+	public function getActionName() {
+
+		return $this->action;
+	}
+	
+	/**
+	 * @method Router::getControllerName()
+	 * Get the name of the currently-set controller which will/has been used to
+	 * handle the request.
+	 *
+	 * @return str
+	 * Returns the result of the controller instance's toString() method.
+	 */
+	public function getControllerName() {
+
+		return (string) $this->controller;
+	}
+
+	/**
+	 * @method Router::serveContent()
+	 * Serve the content set in the $content property.
+	 *
+	 * @return void
+	 * In the case of an asynchronous request, or in the case the $serveContentOnly
+	 * property is set to TRUE, the content contained in the $content property
+	 * will be flushed to the client.
+	 *
+	 * Otherwise, the template file set in the
+	 * $templatePath property will be sent to the client. That template file should
+	 * access the $content property to display the content within the template.
+	 */
+	public function serveContent() {
+
+		if 		($this->request->isAsync()) 		$this->flushContent();
+		elseif	($this->serveContentOnly === true)	$this->flushContent();
+		else	$this->flushAll();
+	}
+
+	/**
+	 * @method serveErrorNotice()
+	 * Serve the Error Page (or deliver an error notice to the client) in the
+	 * event of an unrecoverable (but non-fatal) exception.
+	 *
+	 * @param Exception $e
+	 * The exception which could not be recovered from.
+	 */
+	public function serveErrorNotice($e) {
+
+		if ($this->request->isAsync()) {
+
+			$res	=	new AsyncResponse([
+					'success'	=>	false,
+					'title'		=>	'The server could not process your request.',
+					'message'	=>	$e->getMessage(),
+			]);
+			$res->sendResponse();
+
+		} else {
+
+			switch ($this->request->getNameOfRequestedController()) {
+
+				case 'admin':
+					$this->setTemplate(ADMIN_TEMPLATE);
+					break;
+
+				default:
+					$this->setTemplate(PUBLIC_TEMPLATE);
+			}
+			require_once PUBLIC_PATH . '/view/pages/error.php';
+		}
+		$this->setContent(ob_get_clean());
+		$this->serveContent();
+	}
+
+	/**
+	 * @method Router::setController()
+	 * Specifiy the controller which should handle the request.
+	 *
+	 * @param IRequestController $controller
+	 * The instance of the controller.
+	 * 
+	 * @return void
+	 */
+	public function setController(IRequestController $controller) {
+
+		if (array_key_exists((string) $controller, $this->routes)) {
+
+			$this->controller	=	$controller;
+
+		} else {
+
+			$m = __METHOD__ . ': Invalid controller.';
+			throw new Exception($m, 304);
+		}
+	}
+	
+	/**
+	 * @method Router::setControllerAction()
+	 * Specify the action to be executed by the controller.
+	 *
+	 * @param str $action
+	 * The name of the action, as defined by its method name.
+	 *
+	 * @return void
+	 */
+	public function setControllerAction($action) {
+
+		if (in_array($action, $this->routes[(string) $this->controller])) {
+
+			$this->action = $action;
+
+		} else {
+
+			$message = __METHOD__ . ': Action not valid for selected controller';
+			throw new Exception($message, 305);
+		}
+		
+	}
+	
+	/**
+	 * @method Router::toggleServeContentOnly()
+	 * Toggle or specify the setting which determines whether content should be
+	 * sent to the client within an HTML template or by itself, e.g. in the case
+	 * of the transmission of JSON data.
+	 *
+	 * @param bool $setting
+	 * The desired setting. Defaults to null, which toggles the current setting.
+	 */
+	public function toggleServeContentOnly($setting = null) {
+
+		if ($setting === null) {
+
+			$this->serveContentOnly = ($this->serveContentOnly === false)
+				? true
+				: false;
+		}
+		else {
+
+			$this->serveContentOnly = ($setting) ? true : false;
+		}
+	}
+	
+	/**
+	 * @method loadRoutes()
+	 * Load the list of routes from the database.
+	 * TODO: Having the routes in the database prevents them from being
+	 * included in the version control. Perhaps re-implement with a config file.
+	 *
+	 * @return array
+	 * Returns an associative array of the routes, arranged via the actions'
+	 * controllers.
+	 */
+	private function loadRoutes() {
+
+		$routes		=	[];
+		$actionFlds	=	[new QueryField('action')];
+		$ctrlFlds	=	[new QueryField('controller')];
+
+		$results	=	$this->db->select('actions')
+			->fields([
+				'table'	=>	'actions', 
+				'fields'=>	$actionFlds,
+			])
+			->fields([
+				'table'	=>	'controllers', 
+				'fields'=>	$ctrlFlds,
+			])
+			->joinTable('left', 'controllers', 'actions.controller', '=', 'controllers.id')
+			->execute()
+			->fetchAllResults();
+
+		for ($j=0 ; $j<count($results) ; $j++) {
+
+			$ctrl				=	$results[$j]['controller'];
+			$routes[$ctrl][]	=	$results[$j]['action'];
+		}
+		return $routes;
+	}
+}
+
