5a257dfe6911da358cac23e1041b9443d9328d6e
Jack Brown <jack@jackbrown.io>
2018-10-26 10:07:44 -0400

    Refactored Settings Class into Config Class
    I've combined the Settings class and the Config class because I found
    that they had similar responsibilities. The Settings class was being
    used to load the settings from the config file, and the Config class was
    being used to configure the application. To me, those responsibilities
    aren't different enough to require separate classes.
    
    If I recall correctly, the Config class was requiring the Settings class
    as a dependency. To me, it makes more sense to keep the similar
    responsibilities together under one class.

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -19,23 +19,81 @@
  */
 namespace JackBradford\ActionRouter\Config;
 
+use Cartalyst\Sentinel\Native\Facades\Sentinel;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
 class Config {
+
+	private $action_query_str;
+	private $async_post_flag;
+	private $dbs;
+	private	$controllers;
+	private $ctrl_query_str;
+	private $default_action;
+	private $login_page_path;
+	private	$permissions;
+	private	$templates;
+	private	$users;
+
+	/**
+	 * @method Config::__construct
+	 * Create a new Config instance.
+	 *
+	 * @return Config
+	 *
+	 */
+	public function __construct() {}
+
+	/**
+	 * @method Config::getDirective()
+	 * Access a directive from a specified section.
+	 *
+	 * @param str $section
+	 * The name of the section to access.
+	 *
+	 * @return mixed
+	 * Returns the value (or array) of the specified directive.
+	 *
+	 */
+	public static function getDirective($section) {
+
+		if (!property_exists($this, $section)) {
+
+			$message	=	__METHOD__.': Invalid property: ';
+			$message	.=	$sectionName;
+			throw new Exception($message);
+		}
+
+		return $this->{$section};
+	}
 
 	/**
 	 * @method Config::setConfigurationFromFile
 	 * Configure the router by loading directives from the .ini file.
 	 *
-	 * @param str $iniPath
-	 * The path of the configuration file.
+	 * @param str $file
+	 * The filename of the configuration (.conf.json) file.
 	 *
 	 * @return void
 	 *
 	 */
-	public static function setConfigurationFromFile($iniPath) {
+	public function setConfigurationFromFile($file) {
 
-		Settings::load($iniPath);
+		$this->load($file);
 		$this->loadUserManagementSystem();
 		$this->defineGlobalConstants();
+	}
+
+	/**
+	 * @method Config::unload
+	 * Unload all configuration from the class.
+	 *
+	 * @return void
+	 */
+	public function unload() {
+
+		$properties	=	get_object_vars($this);
+		foreach ($properties as $prop => $val) unset($this->{$prop});
 	}
 
 	/**
@@ -48,9 +106,50 @@ class Config {
 	 */
 	private function defineGlobalConstants() {
 
-		define('CONTROLLERS',		Settings::getDirective('controllers', 'ctrl'));
-		define('TEMPLATES',			Settings::getDirective('templates', 'tmp'));
-		define('DEFAULT_CONTROLLER',Settings::getDirective('controllers', 'default'));
+		$controllers	=	$this->getDirective('controllers');
+		$templates		=	$this->getDirective('templates');
+		$defaultActn	=	$this->getDirective('default_action');
+		$ctrl_qs		=	$this->getDirective('ctrl_query_str');
+		$act_qs			=	$this->getDirective('action_query_srt');
+		$asyncFlag		=	$this->getDirective('async_post_flag');
+		$defaultCtrl	=	$controllers->default;
+		unset($controllers->default);
+
+		define('CONTROLLERS',			(array) $controllers);
+		define('TEMPLATES',				(array) $templates);
+		define('DEFAULT_CONTROLLER',	$defaultCtrl);
+		define('DEFAULT_ACTION',		$defaultActn);
+		define('CTRL_QUERY_STR',		$ctrl_qs);
+		define('ACTION_QUERY_STR',		$act_qs);
+		define('ASYNC_POST_FLAG',		$asyncFlag);
+	}
+
+	/**
+	 * @method Config::load
+	 * Load a configuration file for access via this class.
+	 *
+	 * @param str $file
+	 * The filename of the configuration (.conf.json) file.
+	 *
+	 * @return void
+	 */
+	private function load($file) {
+
+		$settings	=	$this->validateSettings(
+			json_decode(file_get_contents($file))
+		);
+
+		foreach ($settings as $sectionName => $section) {
+
+			if (!property_exists($this, $sectionName)) {
+
+				$message	=	__METHOD__.': Invalid property: ';
+				$message	.=	$sectionName;
+				throw new Exception($message);
+			}
+
+			$this->{$sectionName}	=	$section;
+		}
 	}
 
 	/**
@@ -62,7 +161,74 @@ class Config {
 	 */
 	private function loadUserManagementSystem() {
 
-		require_once dirname(__FILE__) . '/integrate_sentinel.php';
+		$db			=	$this->getDirective('dbs')->sentinel_db;
+		$dbConf		=	$this->getDirective('dbs')->{$db};
+		$capsule	=	new Capsule;
+
+		$capsule->addConnection([
+
+			'driver'	=>	'mysql',
+			'host'		=>	$dbConf->hostname,
+			'database'	=>	$dbConf->database,
+			'username'	=>	$dbConf->username,
+			'password'	=>	$dbConf->password,
+			'charset'	=>	'utf8',
+			'collation'	=>	'utf8_unicode_ci',
+		]);
+
+		$capsule->bootEloquent();
+	}
+
+	/**
+	 * @method Config::parseArgvIntoGet
+	 * Translate request parameters given via the CLI into the $_GET superglobal
+	 * array. This method expects the parameters to be in the form 'param=value',
+	 * with each parameter separated by a space. This method serves to maintain
+	 * the $_GET interface while using the application via the CLI.
+	 *
+	 * @return void
+	 */
+	public function parseArgvIntoGet() {
+
+		global $argv;
+		if (!isset($argv)) return;
+
+		foreach ($argv as $arg) {
+
+			$e	=	explode("=", $arg);
+
+			if (count($e) == 2) {
+
+				$_GET[$e[0]] = $e[1];
+			}
+			else {
+
+				$_GET[$e[0]] = 0;
+			}
+		}
+	}
+
+	/**
+	 * @method Config::validateSettings
+	 * Check whether the settings are in the expected format.
+	 *
+	 * @param obj $settings
+	 * The settings should be in stdClass format.
+	 *
+	 * @return obj
+	 * The method will return the settings given if they are valid. Otherwise,
+	 * the method will throw an exception.
+	 */
+	private function validateSettings($settings) {
+
+		if (!is_object($settings)) {
+
+			$m	=	__METHOD__ . ': Expects object. Check file location and 
+					verify JSON is valid.';
+			throw new Exception($m);
+		}
+
+		return $settings;
 	}
 }
 
