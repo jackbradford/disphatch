0d4c2f320b0e3833a62d8d3b0b62f6a2752d7ad7
Jack Brown <jack@jackbrown.io>
2018-11-05 13:22:54 -0500

    Converted Tabs to Spaces in Config.php, Settings.php
    So far, I've converted the tabs in each of the files in the /Config
    directory to spaces. I've also removed the vertical alignments. These
    practices aim to make reading diffs easier. It should be easier to see
    the parts that have truly changed, and it should stop me from seeing
    whole sections of source highlighted even though only a small change was
    made.

--- src/Config/Settings.php
+++ src/Config/Settings.php
@@ -1,245 +1,95 @@
 <?php
 /**
  * @file
- * This file provides classes which afford access to the directives contained
- * within the application's .ini file.
- *
+ * This file provides the Settings class, which affords access to the
+ * directives contained within a configuration file (e.g. the application's
+ * .conf.json file).
  *
  * @class Settings
- * This class provides a means to load the .ini directives, which is necessary
- * to access them via this class.
- *
- * The class also provides the mechanism through which the scope of those 
- * directives are enforced. That is, directive groups can have their "private"
- * directive set to 1, which serves to restrict access to those directives.
- *
- * @method Settings::getDirective()
- * This method may be invoked in order to access private directives.
- *
- *
- * @class PublicSettings
- * This class extends the Settings class. By instantiating it, one may use the
- * "magic" __get and __isset methods to access the public directives. Using an 
- * instance of this class helps to ensure that private directives will not be 
- * leaked to visitors of the site.
+ * This class provides a means to load JSON config file directives, which is
+ * necessary to access them via this class. The class does not allow the
+ * directives to be changed once they are loaded.
  *
  */
 namespace JackBradford\ActionRouter\Config;
 
-abstract class Settings {
+class Settings {
 
-	static private $privateDirectives = [];
-	static private $publicDirectives = [];
+    private $action_query_str;
+    private $async_post_flag;
+    private $dbs;
+    private $controllers;
+    private $ctrl_query_str;
+    private $default_action;
+    private $templates;
+    private $users;
 
-	/**
-	 * @method Settings::getDirective()
-	 * Access a directive from a specified section.
-	 *
-	 * @param str $section
-	 * A string representation of the name of the section, matching that of the
-	 * corresponding section label in the .ini.
-	 *
-	 * @param str $keys
-	 * One $key parameter can be passed to access a standalone directive in the
-	 * .ini file.
-	 * When accessing an array of directives, specify the array with the first
-	 * $key parameter. Then, to access a specific directive within that array, 
-	 * pass an additional $key parameter which matches the desired array key.
-	 *
-	 * @return mixed
-	 * Returns the value (or array) of the specified directive.
-	 *
-	 */
-	public static function getDirective($section, ...$keys) {
+    /**
+     * @method Settings::__construct
+     *
+     * @return Settings
+     */
+    public function __construct() {}
 
-		if (empty($keys)) {
-			throw new Exception(__METHOD__.' expects at least one key.');
-		}
+    /**
+     * @method Settings::getDirective()
+     * Access a directive from a specified section.
+     *
+     * @param str $section
+     * The name of the section to access.
+     *
+     * @return mixed
+     * Returns the value (or array) of the specified directive.
+     *
+     */
+    public static function getDirective($section) {
 
-		$sectionDirs	=	self::extractSection($section);
+        if (!property_exists($this, $section)) {
 
-		foreach ($keys as $key) {
+            $message = __METHOD__.': Invalid property: ';
+            $message .= $sectionName;
+            throw new Exception($message);
+        }
 
-			$value 		=	self::getDirectiveValue($sectionDirs, $key);
-			if (is_array($value)) $sectionDirs = $value;
-			else break;
-		}
+        return $this->{$section};
+    }
 
-		return $value;
-	}
+    /**
+     * @method Settings::load
+     * Load a configuration file for access via this class.
+     *
+     * @param str $file
+     * The filename of the configuration (.conf.json) file.
+     *
+     * @return void
+     */
+    public function load($file) {
 
-	/**
-	 * @method Settings::load()
-	 * Load a configuration file for access via this class.
-	 *
-	 * @param str $ini
-	 * The filename of the configuration (.ini) file.
-	 *
-	 * @param bool $processSections
-	 * From the manual (php.net):
-	 * [By setting the process_sections parameter to TRUE, you get a multi-
-	 * dimensional array, with the section names and settings included.]
-	 * Here, the default is TRUE.
-	 */
-	public static function load($ini, $processSections = TRUE) {
+        $settings = json_decode(file_get_contents($file));
 
-		$settings	=	self::parseIni($ini, $processSections);
+        foreach ($settings as $sectionName => $section) {
 
-		foreach ($settings as $sectionName => $array) {
+            if (!property_exists($this, $sectionName)) {
 
-			$isPrivate	=	self::sectionIsPrivate($array);
-			$dirs		=	self::unpackSettings($array);
-			self::addDirectivesToSection($sectionName, $dirs, $isPrivate);
-		}
-	}
+                $message = __METHOD__.': Invalid property: ';
+                $message .= $sectionName;
+                throw new Exception($message);
+            }
 
-	/**
-	 * @method Settings::unload()
-	 * Unload all configuration from the class.
-	 */
-	public static function unload()
-	{
-		self::$privateDirectives = [];
-		self::$publicDirectives = [];
-	}
+            $this->{$sectionName} = $section;
+        }
+    }
 
-	/**
-	 * @method __get()
-	 * Access a public directive from a child class.
-	 *
-	 * @param str $key
-	 * The name of the public directive. To access a section within the .ini,
-	 * preface the directive name with the section name, followed immediately
-	 * by a colon. This method supports all multi-dimensional arrays.
-	 * E.g., db_admin:hostname
-	 */
-	public function __get($keys) {
+    /**
+     * @method Settings::unload
+     * Unload all configuration from the class.
+     *
+     * @return void
+     */
+    public function unload() {
 
-		$keys	=	$this->parseKeysArg($keys);
-		$dirSet	=	self::$publicDirectives;
-		foreach ($keys as $key) {
-			if (is_array($dirSet[$key])) {
-				$dirSet	=	$dirSet[$key];
-			}
-			else {
-				$value	=	$dirSet[$key];
-				break;
-			}
-		}
-		if (!isset($value)) {
-			throw new Exception(__METHOD__ . ': No public directive found.');
-		}
-		return $value;
-	}
-
-	/**
-	 * @method __isset()
-	 * Check whether a public directive is accessible.
-	 *
-	 * @param str $key
-	 * The name of the public directive. To access a section within the .ini,
-	 * preface the directive name with the section name, followed immediately
-	 * by a colon. This method supports all multi-dimensional arrays.
-	 * E.g., db_admin:hostname
-	 */
-	public function __isset($key) {
-
-		$keys	=	$this->parseKeysArg($keys);
-		$dirSet	=	self::$publicDirectives;
-		foreach ($keys as $key) {
-			if (isset($dirSet[$key]) && is_array($dirSet[$key])) {
-				$dirSet	=	$dirSet[$key];
-			}
-			else {
-				return isset($dirSet[$key]);
-			}
-		}
-		return true; // Loop stopped on section or directive array.
-	}
-
-	private static function addDirectivesToSection($sectName, $dirs, $private = FALSE) {
-
-		if ($private)	$dir = &self::$privateDirectives;
-		else			$dir = &self::$publicDirectives;
-		$dir[$sectName]	=	$dirs;
-	}
-
-	private static function extractSection($section) {
-
-		if (isset(self::$privateDirectives[$section])) {
-			$sectionDirs = self::$privateDirectives[$section];
-		}
-		elseif (isset(self::$publicDirectives[$section])) {
-			$sectionDirs = self::$publicDirectives[$section];
-		}
-		else {
-			throw new Exception(__METHOD__.': Section '.$section.' not found.');
-		}
-		return $sectionDirs;
-	}
-
-	private static function getDirectiveValue($dirs, $key) {
-
-		$value	=	(isset($dirs[$key])) ? $dirs[$key] : null;
-		if ($value === null) {
-
-			$message	=	__METHOD__ . ": $key not found.";
-			$message	.=	' NULL values are invalid -- simply omit the directive.';
-			throw new Exception($message);
-		}
-		return $value;
-	}
-
-	private static function parseIni($ini, $processSections) {
-
-		$settings = parse_ini_file($ini, $processSections);
-
-		if ($settings === false) {
-
-			throw new Exception(
-				__METHOD__ . ': Could not parse configuration file specified: "' . $ini . '"'
-			);
-		}
-
-		return $settings;
-	}
-
-	private function parseKeysArg($keys) {
-
-		return explode(':', $keys);
-	}
-
-	private static function sectionIsPrivate($settings) {
-
-		return (
-			array_key_exists('private', $settings)
-			&& $settings['private'] === "1"
-		) ? true : false;
-	}
-
-	/**
-	 * @method Settings::unpackSettings()
-	 * Unpack an array of directives into a multidimensional array.
-	 */
-	private static function unpackSettings($array)
-	{
-		$settings = [];
-
-		foreach ($array as $directive => $setting) {
-
-			if (is_array($setting)) {
-
-				$settings[$directive] = self::unpackSettings($directive, $setting);
-			}
-			else {
-
-				$settings[$directive] = $setting;
-			}
-		}
-
-		return $settings;
-	}
+        $properties = get_object_vars($this);
+        foreach ($properties as $prop => $val) unset($this->{$prop});
+    }
 }
-
-class PublicSettings extends Settings {}
 
