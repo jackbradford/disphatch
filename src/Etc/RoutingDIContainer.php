d20daab30c9bfc19188b8e7c54db96ab476e5920
Jack Brown <jack@jackbrown.io>
2018-11-05 16:45:12 -0500

    RoutingDIContainer now requires UserManager on construction.
    Added a new dependency to the RoutingDIContainer: an instance of
    UserManager. It's often necessary to have access to the user's details
    when routing and processing a request, and including it here in the
    dependency injection container serves to provide it to the classes that
    need it without adding dependencies to each of those other classes.
    
    Of course, the tabs have been converted to spaces and the vertical
    alignment has been removed.

--- src/Etc/RoutingDIContainer.php
+++ src/Etc/RoutingDIContainer.php
@@ -9,38 +9,50 @@ namespace JackBradford\ActionRouter\Etc;
 
 class RoutingDIContainer {
 
-	public $request;
-	public $db;
-	public $logger;
-	public $user;
+    public $config;
+    public $db;
+    public $logger;
+    public $request;
+    public $user;
 
-	/**
-	 * @method RoutingDIContainer::__construct()
-	 * Construct a Dependency-Injection container for a Router.
-	 *
-	 * @param Request $req
-	 * An instance of the Request class, which represents the initial request
-	 * sent by the client.
-	 *
-	 * @param Db $db
-	 * An instance of the Db class, which represents a connection to a database.
-	 *
-	 * @param Logger $logger
-	 * An instance of the Logger class, which is responsible for persisting
-	 * errors and other messages to the database and log file(s).
-	 *
-	 * @param UserManager $user
-	 * An instance of the UserManager class, which represents the currently
-	 * logged-in user.
-	 *
-	 * @return RoutingDIContainer
-	 */
-	public function __construct(Request $req, Db $db, Logger $logger, UserManager $user) {
-	
-		$this->request	=	$req;
-		$this->db		=	$db;
-		$this->logger	=	$logger;
-		$this->user		=	$user;
-	}
+    /**
+     * @method RoutingDIContainer::__construct()
+     * Construct a Dependency-Injection container for a Router.
+     *
+     * @param Config $config
+     * An instance of the Config class, which represents the router's
+     * configuration.
+     *
+     * @param Request $req
+     * An instance of the Request class, which represents the initial request
+     * sent by the client.
+     *
+     * @param Db $db
+     * An instance of the Db class, which represents a connection to a database.
+     *
+     * @param Logger $logger
+     * An instance of the Logger class, which is responsible for persisting
+     * errors and other messages to the database and log file(s).
+     *
+     * @param UserManager $user
+     * An instance of the UserManager class, which represents the currently
+     * logged-in user.
+     *
+     * @return RoutingDIContainer
+     */
+    public function __construct(
+        Config $config,
+        Request $req,
+        Db $db,
+        Logger $logger,
+        UserManager $user
+    ) {
+    
+        $this->config = $config;
+        $this->request = $req;
+        $this->db = $db;
+        $this->logger = $logger;
+        $this->user = $user;
+    }
 }
 
