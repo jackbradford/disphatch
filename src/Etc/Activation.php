13ec1add5f8994d7742cb9213f5732cd78019250
Jack Brown <jack@jackbrown.io>
2018-11-06 09:32:01 -0500

    Added new class: Activation
    This class represents an Activation record. The class was created to
    decouple the third-party user-management library from the rest of the
    codebase. For example, when the User class activates a user it can refer
    to the activation via this class, rather than the class used by the
    library. The idea is to make a future replacement of that library easier
    to manage by abstracting its functionality with interfaces for the other
    parts of the system to use.
    
    I've also ensured that spaces have been used instead of tabs.

---
+++ src/Etc/Activation.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * @file Activation.php
+ * This file provides a class to represent an Activation record.
+ *
+ */
+namespace JackBradford\ActionRouter\Etc;;
+
+/**
+ * @class Activation
+ * This class provides a means of interacting with Activation records 
+ * independent of the third-party user management library. That is, if that
+ * library is ever replaced with a different one, a new implementation of this
+ * class can be added, as opposed to finding every place in the codebase where
+ * the first library was called and changing the implementation in each of
+ * those places.
+ *
+ */
+class Activation {
+
+    private $code;
+    private $userId;
+    private $createdAt;
+    private $updatedAt;
+    private $id;
+
+    /**
+     * @method Activation::__construct
+     * Create an instance of the Activation class, which represnets a User
+     * Activation record.
+     *
+     * @param array $data
+     *  ['code']        The activation code.
+     *  ['userId']      The ID of the user being activated.
+     *  ['createdAt']   The time of the activation record's creation.
+     *  ['updatedAt']   The time of the activation record's last update.
+     *  ['id']          The ID of the activation record.
+     *
+     * @return Activation
+     */
+    public function __construct(array $data) {
+
+        if (!$this->validateConstructorInput($data)) {
+
+            throw new Exception(__METHOD__ . ': Missings argument(s).');
+        }
+
+        $this->code = $data['code'];
+        $this->userId = $data['userId'];
+        $this->createdAt = $data['createdAt'];
+        $this->updatedAt = $data['updatedAt'];
+        $this->id = $data['id'];
+    }
+
+    /**
+     * @method Activation::getDetails
+     * Get the Activation record's data.
+     *
+     * @return obj
+     */
+    public function getDetails() {
+
+        return (object) [
+
+            'code' => $this->code,
+            'userId' => $this->user_id,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+            'id' => $this->id,
+        ];
+    }
+
+    private function validateConstructorInput(array $data) {
+
+        if (!isset($data['code'])) return false;
+        if (!isset($data['userId'])) return false;
+        if (!isset($data['createdAt'])) return false;
+        if (!isset($data['updatedAt'])) return false;
+        if (!isset($data['id'])) return false;
+        return true;
+    }
+}
+
