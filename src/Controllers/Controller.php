2c8d6d631d6a3d44deddae51eb337a1ffe59449a
Jack Brown <jack@jackbrown.io>
2018-11-06 14:51:41 -0500

    Added Abstract Class: Controller
    Defined an abstract class to provide basic functionality to the
    request-handling controllers. A user of this system may extend this
    class to use that functionality, but it is not required as long as the
    user's custom-defined controller implements the proper interface.

---
+++ src/Controllers/Controller.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @file Controllers/Controller.php
+ * This file provides an abstract class which serves to standardize the
+ * implementation of controllers for use with the router.
+ *
+ */
+namespace JackBradford\ActionRouter\Controllers;
+
+abstract class Controller {
+
+    protected $config;
+    protected $db;
+    protected $logger;
+    protected $request;
+    protected $response;
+    protected $router;
+    protected $userMgr;
+
+    final public function __construct(Router $router, RoutingDIContainer $dc) {
+
+        $this->router = $router;
+        $this->config = $dc->config;
+        $this->request = $dc->request;
+        $this->logger = $dc->logger;
+        $this->db = $dc->db;
+        $this->userMgr = $dc->user;
+    }
+
+    /**
+     * @method Controller::fromGET
+     * Access a parameter passed to the script via $_GET and return NULL if
+     * the index is not defined. This has the advantage of not throwing a
+     * notice in the error log.
+     *
+     * @param str $index
+     * The index of the $_GET parameter.
+     *
+     * @return mixed
+     * Returns the value of $_GET[$index] or NULL if that index is not defined.
+     */
+    public function fromGET($index) {
+
+        return (isset($_GET[$index])) ? $_GET[$index] : null;
+    }
+
+    /**
+     * @method Controller::setResponse
+     * Commit the result(s) of the action and provide content and/or data with
+     * which to reply to the originator of the request.
+     *
+     * @param ControllerResponse $response
+     * An instance of the ControllerResponse class.
+     *
+     * @return void
+     */
+    final public function setResponse(ControllerResponse $response) {
+
+        $this->response = $response;
+    }
+}
+
