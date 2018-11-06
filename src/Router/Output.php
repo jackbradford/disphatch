d0e5233a5a2d45fef5c7442a35c50bdbd2107d6e
Jack Brown <jack@jackbrown.io>
2018-11-06 09:39:32 -0500

    Updated Output class's Public Interface
    Output::setResponse now takes an instance of ControllerResponse as an
    argument. The ControllerResponse class should be returned from the
    controller processing the request and serves to provide a standard
    interface to responses. Therefore, the responses can be manipulated in
    a predictable way by the Output class.
    
    Tabs have been converted to spaces and the vertical alignment removed.

--- src/Router/Output.php
+++ src/Router/Output.php
@@ -11,201 +11,201 @@
  */
 namespace JackBradford\ActionRouter\Router;
 
+use JackBradford\ActionRouter\Controllers\ControllerResponse;
+
 class Output {
 
-	protected $content		=	null;
-	protected $templatePath	=	null;
-	protected $metaDesc		=	null;
-	protected $title		=	null;
-	protected $section		=	null;
+    protected $config;
+    protected $response;
+    protected $templatePath = null;
+    protected $metaDesc = null;
+    protected $title = null;
+    protected $section = null;
 
-	private function __construct() {}
-	private function __clone() {}
+    private function __construct() {}
+    private function __clone() {}
 
-	/**
-	 * @method Output::setPageInfo()
-	 * Set the data associated with the requested page.
-	 *
-	 * @param array $info
-	 * An array containing the page information. Available nodes include 'title'
-	 * (page title), 'metaDesc' (page meta description), and 'section' (the
-	 * section of the site in which the page exists).
-	 *
-	 * @return void
-	 * Sets the appropriate properties of the class.
-	 */
-	public function setPageInfo($info) {
+    /**
+     * @method Output::setResponse()
+     * Set the content of the page, or the content to be returned to the client
+     * as part of an asynchronous request.
+     *
+     * @param ControllerResponse $response
+     * Contains HTML/JSON/etc. content to be stored until it is output.
+     *
+     * @return void
+     * Sets the instance's $content property.
+     */
+    public function setResponse(ControllerResponse $response) {
 
-		if (
-			!isset($info['title']) 
-			|| !isset($info['metaDesc']) 
-			|| !isset($info['section'])
-		) {
+        $this->response = $response;
+    }
 
-			$m	=	__METHOD__ . " expects the \$info array to include 'title', ";
-			$m	.=	"'metaDesc', and 'section' nodes.";
-			throw new InvalidArgumentException($m, 408);
-		}
+    /**
+     * @method Output::setMetaDesc()
+     * Set the meta description of the page. The HTML template specified should
+     * use this description. Not applicable to asynchronous requests.
+     *
+     * @param str $metaDesc
+     * The page's meta description.
+     *
+     * @return void
+     * Sets the instance's $metaDesc property.
+     */
+    public function setMetaDesc($metaDesc) {
 
-		$this->setTitle($info['title']);
-		$this->setMetaDesc($info['metaDesc']);
-		$this->setSection($info['section']);
-	}
+        if (!is_string($metaDesc)) {
 
-	/**
-	 * @method Output::setContent()
-	 * Set the content of the page, or the content to be returned to the client
-	 * as part of an asynchronous request.
-	 *
-	 * @param str $content
-	 * HTML content to be stored until it is output.
-	 *
-	 * @return void
-	 * Sets the instance's $content property.
-	 */
-	public function setContent($content) {
+            $m = __METHOD__ . ' expects $metaDesc as string.';
+            throw new InvalidArgumentException($m, 406);
+        }
 
-		if (!isset($content)) {
+        $this->metaDesc = htmlspecialchars($metaDesc);
+    }
 
-			$m	=	__METHOD__ . ' Missing Argument: expects $content.';
-			throw new BadMethodCallException($m, 403);
-		}
+    /**
+     * @method Output::setPageInfo()
+     * Set the data associated with the requested page.
+     *
+     * @param array $info
+     * An array containing the page information. Available nodes include 'title'
+     * (page title), 'metaDesc' (page meta description), and 'section' (the
+     * section of the site in which the page exists).
+     *
+     * @return void
+     * Sets the appropriate properties of the class.
+     */
+    public function setPageInfo(array $info) {
 
-		$this->content = $content;
-	}
+        if (
+            !isset($info['title']) 
+            || !isset($info['metaDesc']) 
+            || !isset($info['section'])
+        ) {
 
-	/**
-	 * @method Output::setTemplate()
-	 * Set the HTML template file to be used when serving content as part of a
-	 * synchronous page request.
-	 *
-	 * @param str $templatePath
-	 * The filepath of the HTML template to be used to serve the content.
-	 *
-	 * @return void
-	 * Sets the instance's $templatePath property.
-	 */
-	public function setTemplate($templatePath) {
+            $m = __METHOD__ . " expects the \$info array to include 'title', ";
+            $m .= "'metaDesc', and 'section' nodes.";
+            throw new InvalidArgumentException($m, 408);
+        }
 
-		if (!is_string($templatePath)) {
+        $this->setTitle($info['title']);
+        $this->setMetaDesc($info['metaDesc']);
+        $this->setSection($info['section']);
+    }
 
-			$m = __METHOD__ . ' expects argument $templatePath as string.';
-			throw new InvalidArgumentException($m, 404);
-		}
+    /**
+     * @method Output::setSection()
+     * Set the current section of the site in which the page exists. The HTML
+     * template specified will use this section. Not applicable to asynchronous
+     * requests.
+     *
+     * @param str $section
+     * The site section in which the requested page exists.
+     *
+     * @return void
+     * Set the instance's $section property.
+     */
+    public function setSection($section) {
 
-		$this->templatePath	= $templatePath;
-	}
+        if (!is_string($section)) {
 
-	/**
-	 * @method Output::setTitle()
-	 * Set the title of the page. The HTML template specified will use this
-	 * title. Not applicable to asynchronous requests.
-	 *
-	 * @param str $title
-	 * The title of the page.
-	 *
-	 * @return void
-	 * Sets the instance's $title property.
-	 */
-	public function setTitle($title) {
+            $m = __METHOD__ . ' expects $section as string.';
+            throw new InvalidArgumentException($m, 407);
+        }
 
-		if (!is_string($title)) {
+        $this->section = htmlspecialchars($section);
+    }
 
-			$m = __METHOD__ . ' expects $title as string.';
-			throw new InvalidArgumentException($m, 405);
-		}
+    /**
+     * @method Output::setTemplate()
+     * Set the HTML template file to be used when serving content as part of a
+     * synchronous page request.
+     *
+     * @param str $templatePath
+     * The filepath of the HTML template to be used to serve the content.
+     *
+     * @return void
+     * Sets the instance's $templatePath property.
+     */
+    public function setTemplate($templatePath) {
 
-		$this->title = htmlspecialchars($title);
-	}
+        if (!is_string($templatePath)) {
 
-	/**
-	 * @method Output::setMetaDesc()
-	 * Set the meta description of the page. The HTML template specified should
-	 * use this description. Not applicable to asynchronous requests.
-	 *
-	 * @param str $metaDesc
-	 * The page's meta description.
-	 *
-	 * @return void
-	 * Sets the instance's $metaDesc property.
-	 */
-	public function setMetaDesc($metaDesc) {
+            $m = __METHOD__ . ' expects argument $templatePath as string.';
+            throw new InvalidArgumentException($m, 404);
+        }
 
-		if (!is_string($metaDesc)) {
+        $this->templatePath = $templatePath;
+    }
 
-			$m = __METHOD__ . ' expects $metaDesc as string.';
-			throw new InvalidArgumentException($m, 406);
-		}
+    /**
+     * @method Output::setTitle()
+     * Set the title of the page. The HTML template specified will use this
+     * title. Not applicable to asynchronous requests.
+     *
+     * @param str $title
+     * The title of the page.
+     *
+     * @return void
+     * Sets the instance's $title property.
+     */
+    public function setTitle($title) {
 
-		$this->metaDesc = htmlspecialchars($metaDesc);
-	}
+        if (!is_string($title)) {
 
-	/**
-	 * @method Output::setSection()
-	 * Set the current section of the site in which the page exists. The HTML
-	 * template specified will use this section. Not applicable to asynchronous
-	 * requests.
-	 *
-	 * @param str $section
-	 * The site section in which the requested page exists.
-	 *
-	 * @return void
-	 * Set the instance's $section property.
-	 */
-	public function setSection($section) {
+            $m = __METHOD__ . ' expects $title as string.';
+            throw new InvalidArgumentException($m, 405);
+        }
 
-		if (!is_string($section)) {
+        $this->title = htmlspecialchars($title);
+    }
 
-			$m = __METHOD__ . ' expects $section as string.';
-			throw new InvalidArgumentException($m, 407);
-		}
+    /**
+     * TODO: adapt to ControllerResponse
+     * @method Output::flushContent()
+     * Flush the content set in the $content property to the client. Flushes
+     * only the content, without any template.
+     *
+     * @return void
+     * Sends the contents of the instance's $content property to the client.
+     */
+    protected function flushContent() {
 
-		$this->section = htmlspecialchars($section);
-	}
+        if (empty($this->response)) {
 
-	/**
-	 * @method Output::flushContent()
-	 * Flush the content set in the $content property to the client. Flushes
-	 * only the content, without any template.
-	 *
-	 * @return void
-	 * Sends the contents of the instance's $content property to the client.
-	 */
-	protected function flushContent() {
+            $m = __METHOD__ . ': No content found.';
+            throw new BadMethodCallException($m, 408);
+        }
 
-		if (empty($this->content)) {
+        // TODO flush which content?
+        echo $this->content;
+    }
 
-			$m = __METHOD__ . ': No content found.';
-			throw new BadMethodCallException($m, 408);
-		}
+    /**
+     * TODO: adapt to ControllerResponse
+     * @method Output::flushAll()
+     * Flush the content set in the $content property within the template set in
+     * the $templatePath property to the client.
+     *
+     * @return void
+     * Sends the template, along with the contents of the instance's $content
+     * property, to the client.
+     */
+    protected function flushAll() {
 
-		echo $this->content;
-	}
+        if (empty($this->templatePath)) {
 
-	/**
-	 * @method Output::flushAll()
-	 * Flush the content set in the $content property within the template set in
-	 * the $templatePath property to the client.
-	 *
-	 * @return void
-	 * Sends the template, along with the contents of the instance's $content
-	 * property, to the client.
-	 */
-	protected function flushAll() {
+            $m = __METHOD__ . ': No template path found.';
+            throw new BadMethodCallException($m, 401);
+        }
 
-		if (empty($this->templatePath)) {
+        if (empty($this->content)) {
 
-			$m	=	__METHOD__ . ': No template path found.';
-			throw new BadMethodCallException($m, 401);
-		}
+            $m = __METHOD__ . ': No content found.';
+            throw new BadMethodCallException($m, 402);
+        }
 
-		if (empty($this->content)) {
-
-			$m	=	__METHOD__ . ': No content found.';
-			throw new BadMethodCallException($m, 402);
-		}
-
-		require_once $this->templatePath;
-	}
+        require_once $this->templatePath;
+    }
 }
 
