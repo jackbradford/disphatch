7759fd22af612d3f3ddba9f3306266e8374b4819
Jack Brown <jack@jackbrown.io>
2018-11-08 09:23:59 -0500

    Reformatted PreparedStatment.php, QueryField.php
    Removed trailing whitespace, converted tabs to spaces, removed vertical
    alignment.

--- src/Db/PreparedStatement.php
+++ src/Db/PreparedStatement.php
@@ -12,65 +12,65 @@ namespace JackBradford\ActionRouter\Db;
 
 class PreparedStatement {
 
-	private $params, $query;
+    private $params, $query;
 
-	/**
-	 * @method PreparedStatement::__construct()
-	 * Create a representation of a prepared statement.
-	 *
-	 * @param str $query
-	 * The prepared query string.
-	 *
-	 * @param array $params
-	 * The set of parameters, ordered as they appear in the query.
-	 *
-	 * @return PreparedStatment
-	 */
-	public function __construct($query, array $params) {
+    /**
+     * @method PreparedStatement::__construct()
+     * Create a representation of a prepared statement.
+     *
+     * @param str $query
+     * The prepared query string.
+     *
+     * @param array $params
+     * The set of parameters, ordered as they appear in the query.
+     *
+     * @return PreparedStatment
+     */
+    public function __construct($query, array $params) {
 
-		try {
+        try {
 
-			$this->validateQuery($query);
-			$this->query	=	$query;
-			$this->params	=	$params;
-		}
-		catch (Exception $e) {
+            $this->validateQuery($query);
+            $this->query = $query;
+            $this->params = $params;
+        }
+        catch (Exception $e) {
 
-			$m	=	__METHOD__ . ' error: ' . $e->getMessage();
-			throw new Exception($m, 0, $e);
-		}
-	}
+            $m = __METHOD__ . ' error: ' . $e->getMessage();
+            throw new Exception($m, 0, $e);
+        }
+    }
 
-	/**
-	 * @method PreparedStatement::getParams()
-	 * Get the set of params.
-	 *
-	 * @return array
-	 */
-	public function getParams() {
+    /**
+     * @method PreparedStatement::getParams()
+     * Get the set of params.
+     *
+     * @return array
+     */
+    public function getParams() {
 
-		return $this->params;
-	}
+        return $this->params;
+    }
 
-	/**
-	 * @method PreparedStatement::getQuery()
-	 * Get the query string.
-	 *
-	 * @return str
-	 */
-	public function getQuery() {
+    /**
+     * @method PreparedStatement::getQuery()
+     * Get the query string.
+     *
+     * @return str
+     */
+    public function getQuery() {
 
-		return $this->query;
-	}
+        return $this->query;
+    }
 
-	private function validateQuery($query) {
+    private function validateQuery($query) {
 
-		if (!is_string($query)) {
+        if (!is_string($query)) {
 
-			$m	=	__METHOD__ .': Query must be a string.';
-			throw new Exception ($m);
-		}
-		return true;
-	}
+            $m = __METHOD__ .': Query must be a string.';
+            throw new Exception ($m);
+        }
+        return true;
+    }
 }
 
--- src/Db/QueryField.php
+++ src/Db/QueryField.php
@@ -5,7 +5,7 @@
  *
  * @class QueryField
  * This class is responsible for representing the specifications of a field
- * in a database query. 
+ * in a database query.
  *
  * Such an entity will always include the name of the field as it appears in
  * the database. The context in which this class is used (i.e., the type of
@@ -17,55 +17,55 @@ namespace JackBradford\ActionRouter\Db;
 
 class QueryField {
 
-	public $field, $alias, $value;
+    public $field, $alias, $value;
 
-	/**
-	 * @method QueryField::__construct()
-	 * Create a new instance of QueryField to represent a field in a
-	 * query.
-	 *
-	 * @param str $field
-	 * The name of the field as it appears in the database.
-	 *
-	 * @param str $alias
-	 * The alias to assign the field. This will affect e.g. the columns
-	 * returned from a SELECT statement.
-	 *
-	 * @param mixed $value
-	 * The value to assign the field. This is necessary for e.g. INSERT
-	 * and UPDATE statements.
-	 *
-	 * @return QueryField
-	 */
-	public function __construct($field, $alias = null, $value = null) {
+    /**
+     * @method QueryField::__construct()
+     * Create a new instance of QueryField to represent a field in a
+     * query.
+     *
+     * @param str $field
+     * The name of the field as it appears in the database.
+     *
+     * @param str $alias
+     * The alias to assign the field. This will affect e.g. the columns
+     * returned from a SELECT statement.
+     *
+     * @param mixed $value
+     * The value to assign the field. This is necessary for e.g. INSERT
+     * and UPDATE statements.
+     *
+     * @return QueryField
+     */
+    public function __construct($field, $alias = null, $value = null) {
 
-		$this->field	=	$field;
-		$this->alias	=	$alias;
-		$this->value	=	$value;
-	}
+        $this->field = $field;
+        $this->alias = $alias;
+        $this->value = $value;
+    }
 
-	/**
-	 * @method QueryField::hasAlias()
-	 * Check whether the field was assigned an alias.
-	 *
-	 * @return bool
-	 */
-	public function hasAlias() {
+    /**
+     * @method QueryField::hasAlias()
+     * Check whether the field was assigned an alias.
+     *
+     * @return bool
+     */
+    public function hasAlias() {
 
-		return ($this->alias === null) ? false : true;
-	}
+        return ($this->alias === null) ? false : true;
+    }
 
-	/**
-	 * @method QueryField::hasValue()
-	 * Check whether the field was assigned a value.
-	 *
-	 * @return bool
-	 * The alias is initialized with a null value. If the value is null,
-	 * returns false.
-	 */
-	public function hasValue() {
+    /**
+     * @method QueryField::hasValue()
+     * Check whether the field was assigned a value.
+     *
+     * @return bool
+     * The alias is initialized with a null value. If the value is null,
+     * returns false.
+     */
+    public function hasValue() {
 
-		return ($this->value === null) ? false : true;
-	}
+        return ($this->value === null) ? false : true;
+    }
 }
 
