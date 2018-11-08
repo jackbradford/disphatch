68b70075ea7d96ed44df8580a749a78b5b23db55
Jack Brown <jack@jackbrown.io>
2018-11-08 09:08:23 -0500

    Reformatted file DbFactory.php
    Trailing whitespace has been removed, vertial alignment has been
    removed, and tabs have been converted to spaces.

--- src/Db/DbFactory.php
+++ src/Db/DbFactory.php
@@ -6,7 +6,7 @@
  *
  * @class DbFactory
  * This class provides the aforementioned factories. It depends on its parent,
- * the Settings class, and its methods return instances of the database-access 
+ * the Settings class, and its methods return instances of the database-access
  * abstraction class.
  *
  */
@@ -14,14 +14,14 @@ namespace JackBradford\ActionRouter\Db;
 
 class DbFactory extends Settings {
 
-	public static function getDbInst() {
+    public static function getDbInst() {
 
-		$qp		=	new MySqlQueryPreparer();
-		$host	=	parent::getDirective('db', 'hostname');
-		$pass	=	parent::getDirective('db', 'password');
-		$db		=	parent::getDirective('db', 'database');
-		$user	=	parent::getDirective('db', 'username');
-		return new Db($host, $pass, $db, $user, $qp);
-	}
+        $qp = new MySqlQueryPreparer();
+        $host = parent::getDirective('db', 'hostname');
+        $pass = parent::getDirective('db', 'password');
+        $db = parent::getDirective('db', 'database');
+        $user = parent::getDirective('db', 'username');
+        return new Db($host, $pass, $db, $user, $qp);
+    }
 }
 
