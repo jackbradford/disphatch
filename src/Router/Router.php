497d416134185a866c97938df3d36ccc7840a43e
Jack Brown <jack@jackbrown.io>
2018-11-27 11:29:03 -0500

    Added 'use' statement to Router, updated calls to controllers
    When attempting to instantiate request-response controllers, Router was
    looking for the classes in the wrong namespace. A 'use' statment has
    been added and the namespace alias added to the controller's name before
    being instantiated.

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -13,6 +13,7 @@
 namespace JackBradford\ActionRouter\Router;
 
 use JackBradford\ActionRouter\Config\Config;
+use JackBradford\ActionRouter\Controllers;
 use JackBradford\ActionRouter\Controllers\ControllerResponse;
 use JackBradford\ActionRouter\Etc\Request;
 use JackBradford\ActionRouter\Etc\UserManager;
@@ -158,8 +159,9 @@ class Router extends Output {
 
         try {
 
-            $ctrlrName = $this->request->getClassNameOfRequestedController();
-            $this->setController(new $ctrlrName($this, $this->dc));
+            $ctrlrName = 'Controllers\\' . $this->request
+                ->getClassNameOfRequestedController();
+            $this->setController(new ctrlrName($this, $this->dc));
             $this->setControllerAction($this->request->getNameOfRequestedAction());
             $this->authorizeRequest();
 
