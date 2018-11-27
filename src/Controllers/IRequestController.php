e2791f8f5a31e966f93c852caf891bab9f5ee7b7
Jack Brown <jack@jackbrown.io>
2018-11-27 11:48:52 -0500

    Added 'use' statements to IRequestController
    This should have been added as part of the last commit, which ended up
    making the child clases of Controller incompatible with the aforementioned
    interface.

--- src/Controllers/IRequestController.php
+++ src/Controllers/IRequestController.php
@@ -6,6 +6,9 @@
  */
 namespace JackBradford\ActionRouter\Controllers;
 
+use JackBradford\ActionRouter\Etc\RoutingDIContainer;
+use JackBradford\ActionRouter\Router\Router;
+
 interface IRequestController {
 
     public function __construct(Router $router, RoutingDIContainer $dc);
