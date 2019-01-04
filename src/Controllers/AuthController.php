6bbaf5233dda0c579947ad6a544dc90dfd61a774
Jack Brown <jack@jackbrown.io>
2019-01-04 12:51:24 -0500

    Added AuthController
    AuthController is a predefined request-fulfillment controller. It has
    one method for handling user authentication requests.

---
+++ src/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @file Controllers/AuthController.php
+ * This file provides the controller which handles authorization requests.
+ *
+ */
+namespace JackBradford\ActionRouter\Controllers;
+
+class AuthControllers extends Controller implements IRequestController {
+
+    public function __toString() {
+
+        return 'auth';
+    }
+
+    public function auth() {
+
+        $data = json_decode($this->fromGET('data'));
+        $cred = [
+
+            'un' => $data['un'],
+            'pw' => $data['pw'],
+        ];
+        try {
+
+            $this->userMgr->login($cred);
+            $success = true;
+            $message = 'Login successful.';
+        }
+        catch (\Exception $e) {
+
+            $success = false;
+            $message = 'Login failed. ' . $e->getMessage();
+        }
+        return new ControllerResponse($success, $message);
+    }
+}
+
